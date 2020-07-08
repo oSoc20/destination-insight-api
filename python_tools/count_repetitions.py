d2f742aaea33dc455392038975328cee9f4121d6
Rodrigo Christiansen <rodrigo.chr@gmail.com>
2020-07-08 16:27:00 +0200

    add script file for origin destination pairs and command line arguments for both scripts

--- python_tools/count_repetitions.py
+++ python_tools/count_repetitions.py
@@ -2,7 +2,15 @@ import pandas as pd
 from misc import count_repetitions
 import sys
 
+# this is script will return a json table with the top or bottom stations by number of searches in origin or destination
+# arg1: {'origin', 'destination'}
+# arg2: {'top', 'bottom'}
+# arg3: integer for number of rows to include
+
+# read local data (for now)
 data = pd.read_csv('data.csv')
-result = count_repetitions(data, 'origin', 'top', 10, return_json=True)
+
+# return requested table
+result = count_repetitions(data, sys.argv[1], sys.argv[2], int(sys.argv[3]), return_json=True)
 print(result)
 sys.stdout.flush()
