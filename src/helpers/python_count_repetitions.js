94d7fb6522a0bf967084fa53aad9638be0c4c8a9
BelgianSoviet <andrewsleruth@gmail.com>
2020-07-14 17:36:09 +0200

    implemented count destination and origin routes

--- src/helpers/python_count_repetitions.js
+++ src/helpers/python_count_repetitions.js
@@ -1,11 +1,30 @@
 
-module.exports = () => new Promise((resolve, reject) => {
+module.exports = (arg1, arg2, arg3, arg4, arg5, arg6) => new Promise((resolve, reject) => {
   const spawn = require('child_process').spawn;
+
+  // spawn(command, [arg1, args], {options})
+  // spawn(<string>, string[], Object)
+  // arg1 is the script we want to run
+  // args are aditional arguments all separated by a ","
+  // options is, well, aditional options
+
+  //  this script will return a json table with the top or bottom stations by number of searches in origin or destination
+  //  arg1: station type {'origin', 'destination'}
+  //  arg2: start date, inclusive (YYYY-MM-DD)
+  //  arg3: end date, inclusive (YYYY-MM-DD)
+  //  arg4: date type {'travel', 'request'}
+  //  arg5: most or least common stations {'top', 'bottom'}
+  //  arg6: integer for number of rows to include
+
+  //  command example: python count_repetitions.py origin 2020-05-13 2020-05-14 travel top 10
+
   // call the python script from the script's directory (cwd)
-  const pythonScript = spawn('python',['count_repetitions.py'], {cwd: 'python_tools'}); // this is where the python script is located
+  const pythonScript = spawn('python',['./count_repetitions.py', arg1, arg2, arg3, arg4, arg5, arg6], {cwd: 'python_tools'})
+    .on('error', (err) => {throw err}); // this is where the python script is located
   pythonScript.stdout.on('data', (data) => {
     resolve(data.toString());
   });
+
   pythonScript.stderr.on('data', (data) => {
     reject(data.toString());
     return;
--- src/routes/data/index.js
+++ src/routes/data/index.js
@@ -13,6 +13,11 @@ module.exports = router
 
   .get('/', async (req, res, next) => {
     // Establish a connection with the mySQL database, check connection.js for more info
+
+    const m = new Date();
+    const lastMonthDateString = m.getUTCFullYear() + "/" + ("0" + (m.getUTCMonth())).slice(-2) + "/" + ("0" + m.getUTCDate()).slice(-2);
+    console.log(lastMonthDateString)
+
     const conn = await connection(dbConfig)
       .catch((err) => {
         console.log(err);
@@ -50,9 +55,63 @@ module.exports = router
     };
 
   })
-  .get('/repetitions', (req, res, next) => {
+  .get('/cntOrig', (req, res, next) => {
+    // sends a response with the top X origins searched
+
+    const m = new Date();
+    const dateString = m.getUTCFullYear() + "/" + ("0" + (m.getUTCMonth()+1)).slice(-2) + "/" + ("0" + m.getUTCDate()).slice(-2);
+    console.log(dateString);
+
+    const lastMonthDateString = m.getUTCFullYear() + "/" + ("0" + (m.getUTCMonth())).slice(-2) + "/" + ("0" + m.getUTCDate()).slice(-2);
+    console.log(lastMonthDateString);
+
     // The data sent in the response is provided by a python script, check python_count_repetitions.js for more info
-    countRepetitions().then((data) => {
+    //  arg1: station type {'origin', 'destination'}
+    //  arg2: start date, inclusive (YYYY-MM-DD)
+    //  arg3: end date, inclusive (YYYY-MM-DD)
+    //  arg4: date type {'travel', 'request'}
+    //  arg5: most or least common stations {'top', 'bottom'}
+    //  arg6: integer for number of rows to include
+    countRepetitions(
+      'origin',
+      req.arg2 || lastMonthDateString,
+      req.arg3 || dateString,
+      req.arg4 || 'travel',
+      req.arg5 || 'top',
+      req.arg6 || '10'
+    ).then((data) => {
+      console.log(data)
+      res.json(data);
+    }).catch((err) => {
+      console.log(err);
+      next(err);
+    });
+  })
+  .get('/cntDest', (req, res, next) => {
+    // sends a response with the top X destinations searched
+
+    const m = new Date();
+    const dateString = m.getUTCFullYear() + "/" + ("0" + (m.getUTCMonth()+1)).slice(-2) + "/" + ("0" + m.getUTCDate()).slice(-2);
+    console.log(dateString);
+
+    const lastMonthDateString = m.getUTCFullYear() + "/" + ("0" + (m.getUTCMonth())).slice(-2) + "/" + ("0" + m.getUTCDate()).slice(-2);
+    console.log(lastMonthDateString);
+
+    // The data sent in the response is provided by a python script, check python_count_repetitions.js for more info
+    //  arg1: station type {'origin', 'destination'}
+    //  arg2: start date, inclusive (YYYY-MM-DD)
+    //  arg3: end date, inclusive (YYYY-MM-DD)
+    //  arg4: date type {'travel', 'request'}
+    //  arg5: most or least common stations {'top', 'bottom'}
+    //  arg6: integer for number of rows to include
+    countRepetitions(
+      'destination',
+      req.arg2 || lastMonthDateString,
+      req.arg3 || dateString,
+      req.arg4 || 'travel',
+      req.arg5 || 'top',
+      req.arg6 || '10'
+    ).then((data) => {
       console.log(data)
       res.json(data);
     }).catch((err) => {
