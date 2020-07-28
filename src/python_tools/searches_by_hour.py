2e75710cc3ca4e31e1eae6c9247b5c6e2beb0ae5
RodrigoChT <rodrigo.chr@gmail.com>
2020-07-28 16:25:07 +0200

    try fix json

--- src/python_tools/searches_by_hour.py
+++ src/python_tools/searches_by_hour.py
@@ -1,6 +1,7 @@
 from funcs.reports import searches_by_hour
 import sys
-
+import json
+import numpy as np
 # this is script will return a json with the amount of searches by hour and day of the week (total)
 # arg1: start date, inclusive (YYYY-MM-DD)
 # arg2: end date, inclusive (YYYY-MM-DD)
@@ -8,10 +9,14 @@ import sys
 
 # command example: python searches_by_hour.py 2020-01-01 2021-01-01 travel
 
+def convert(o):
+    if isinstance(o, np.int64): return int(o)
+    raise TypeError
+
 # return requested table
 result = searches_by_hour(start=sys.argv[1],
                           end=sys.argv[2],
                           time_type=sys.argv[3])
 
-print(result)
+print(json.dumps(result, default=convert))
 sys.stdout.flush()
